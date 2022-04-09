dbeb1aabf66b8d6ce1b3c1bec648819feb832fb7
zlaffey <zacklaffey@gmail.com>
2022-04-09 00:24:08 -0500

    Initial commit

---
+++ dashboard.js
@@ -0,0 +1,74 @@
+const ctx = document.getElementById("myChart");
+const myChart = new Chart(ctx, {
+	type: "bar",
+	data: {
+		labels: [],
+		datasets: [
+			{
+				data: [],
+				backgroundColor: "#198754",
+				borderColor: "#198754",
+				borderWidth: 1,
+			},
+		],
+	},
+	options: {
+		scales: {
+			yAxes: [
+				{
+					ticks: {
+						beginAtZero: true,
+					},
+				},
+			],
+		},
+		legend: {
+			display: false,
+		},
+	},
+});
+
+document.querySelector("#logSubmit").addEventListener("click", updateLog);
+
+function createDataItem(data) {
+	let td = document.createElement("td");
+	td.textContent = `${data}`;
+	return td;
+}
+
+function addData(chart, label, data) {
+	chart.data.labels.push(label);
+	chart.data.datasets.forEach((dataset) => {
+		dataset.data.push(data);
+	});
+	chart.update();
+}
+
+function updateLog() {
+	const log = document.querySelector("#netWorthLog");
+	let dateData = new Date().toLocaleDateString();
+	let cashInput = Number(document.querySelector("#cashInput").value);
+	let savingsInput = Number(document.querySelector("#savingsInput").value);
+	let investmentInput = Number(
+		document.querySelector("#investmentInput").value
+	);
+	let debtInput = Number(document.querySelector("#debtInput").value);
+	let netWorth = Number(cashInput + savingsInput + investmentInput - debtInput);
+
+	let newRow = document.createElement("tr");
+	newRow.appendChild(createDataItem(dateData));
+	newRow.appendChild(createDataItem("$" + cashInput));
+	newRow.appendChild(createDataItem("$" + savingsInput));
+	newRow.appendChild(createDataItem("$" + investmentInput));
+	newRow.appendChild(createDataItem("$" + debtInput));
+	newRow.appendChild(createDataItem("$" + netWorth));
+
+	log.prepend(newRow);
+
+	document.querySelector("#cashInput").value = "";
+	document.querySelector("#savingsInput").value = "";
+	document.querySelector("#investmentInput").value = "";
+	document.querySelector("#debtInput").value = "";
+
+	addData(myChart, dateData, netWorth);
+}
